5089aac956772a2eece29013f8cf65c06ea5dcaa
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-26 10:19:36 +0200

    Correction

--- src/components/Box.js
+++ src/components/Box.js
@@ -8,7 +8,7 @@ class Box extends React.Component {
     render() {
         return (
             <div className="box col-sm-3 col-6">
-                <span class="material-icons" style={{color : `${this.props.color}`}}>
+                <span className="material-icons" style={{color : `${this.props.color}`}}>
                     {this.props.icon}
                 </span>
                 <p>{this.props.value} {this.props.unit} </p>
